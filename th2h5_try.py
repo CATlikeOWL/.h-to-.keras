5cb15a4044d7a04687dde0e490b3e52787ad5607
CATlikeOWL <127040053+CATlikeOWL@users.noreply.github.com>
2025-08-24 07:49:06 +0000

    Save all python files

---
+++ th2h5_try.py
@@ -0,0 +1,30 @@
+import os
+
+# 查看當前工作目錄
+cwd = os.getcwd()
+print("目前位置:", cwd)
+print("目前目錄檔案列表:", os.listdir(cwd))
+
+# 轉檔程式：將舊模型存成新版可用的 .keras 格式
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+print(tf.__version__)  # 應該是 2.12.0
+
+# Step 0: 載入舊模型
+old_model_path = "model105QQ_2_f2"  # 你的舊檔案路徑
+model = load_model(old_model_path)
+
+# Step 1: 建立 output 資料夾（如果還沒存在）
+output_dir = "output"
+os.makedirs(output_dir, exist_ok=True)
+
+# Step 2: 存
+model_filename = "model105QQ_2_f2_converted.h5"
+new_model_path = os.path.join(output_dir, model_filename)
+model.save(new_model_path)
+
+print(f"OK~ 模型已儲存為 H5: {new_model_path}")
+
+#git add output/model105QQ_2_f2_converted.keras
+#git commit -m "Add converted model to output folder"
+#git push
