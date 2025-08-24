c7b24ef1d71921ee2bb5aa832a36c1bdb9a97241
CATlikeOWL <127040053+CATlikeOWL@users.noreply.github.com>
2025-08-24 07:57:29 +0000

    Save all python files

--- th2h5_try.py
+++ th2h5_try.py
@@ -18,13 +18,14 @@ model = load_model(old_model_path)
 output_dir = "output"
 os.makedirs(output_dir, exist_ok=True)
 
-# Step 2: 存
+# Step 2: 存成 Keras v3 推薦的 .keras 格式
 model_filename = "model105QQ_2_f2_converted.h5"
 new_model_path = os.path.join(output_dir, model_filename)
-model.save(new_model_path)
 
-print(f"OK~ 模型已儲存為 H5: {new_model_path}")
+model.save(new_model_path, save_format="h5")
 
+print(f"OK~ 模型已轉存完成: {new_model_path}")
 #git add output/model105QQ_2_f2_converted.keras
 #git commit -m "Add converted model to output folder"
 #git push
+#ls -lh output/ #看成功?
